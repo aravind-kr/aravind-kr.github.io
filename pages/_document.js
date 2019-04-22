cfe56ddddcb405490229d0db8bf5093f482d61a4
aravind-kr <arvindsmvec@gmail.com>
2019-04-22 23:06:35 +1000

    css server side loading changes

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,15 @@
 import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
+import PropTypes from 'prop-types'
+import flush from 'styled-jsx/server'
 
-export default class MyDocument extends Document {
-    static getInitialProps({ renderPage }) {
-        const sheet = new ServerStyleSheet();
-        const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
-        const styleTags = sheet.getStyleElement();
-        return { ...page, styleTags };
-    }
+class MyDocument extends Document {
+    // static getInitialProps({ renderPage }) {
+    //     const sheet = new ServerStyleSheet();
+    //     const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+    //     const styleTags = sheet.getStyleElement();
+    //     return { ...page, styleTags };
+    // }
 
     render() {
         return (
@@ -21,3 +23,44 @@ export default class MyDocument extends Document {
         );
     }
 }
+
+MyDocument.getInitialProps = ctx => {
+    // Render app and page and get the context of the page with collected side effects.
+    let pageContext
+    const page = ctx.renderPage(Component => {
+        const WrappedComponent = props => {
+            pageContext = props.pageContext
+            return <Component {...props} />
+        }
+
+        WrappedComponent.propTypes = {
+            pageContext: PropTypes.object.isRequired,
+        }
+
+        return WrappedComponent
+    })
+
+    let css
+    // It might be undefined, e.g. after an error.
+    if (pageContext) {
+        css = pageContext.sheetsRegistry.toString()
+    }
+
+    return {
+        ...page,
+        pageContext,
+        // Styles fragment is rendered after the app and page rendering finish.
+        styles: (
+            <React.Fragment>
+                <style
+                    id="jss-server-side"
+                    // eslint-disable-next-line react/no-danger
+                    dangerouslySetInnerHTML={{ __html: css }}
+                />
+                {flush() || null}
+            </React.Fragment>
+        ),
+    }
+}
+
+export default MyDocument
