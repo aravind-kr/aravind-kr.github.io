e43a43bb2867a9f8e4abb0dd735947689e52e266
Aravind <arvindsmvec@gmail.com>
2019-06-10 15:24:31 +1000

    fs module changes handles in next js config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const webpack = require('webpack')
 module.exports = {
     webpack(config) {
         config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
+        // Fixes npm packages that depend on `fs` module
+        config.node = {
+            fs: 'empty',
+        }
         return config
     },
     // assetPrefix: process.env.NODE_ENV === 'production' ? '/{reponame}' : '',
